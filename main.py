764297cc3cf9ba5336d6f1cadf13d4db23494e45
Zhang <longqi90@gmail.com>
2015-06-22 20:46:44 +0800

    adding POST function and wechat-sdk

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ longqi 9/6/15 22:11
 import tornado.ioloop
 import tornado.web
 import hashlib
+from wechat_sdk import WechatBasic
 
 from tornado.options import define, options
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=80, help="run on the given port", type=int)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello, world")
+
+
+token = 'test'
+wechat = WechatBasic(token=token)
 
 
 class WechatHandler(tornado.web.RequestHandler):
@@ -25,9 +30,7 @@ class WechatHandler(tornado.web.RequestHandler):
         timestamp = self.get_argument('timestamp')
         nonce = self.get_argument('nonce')
         echostr = self.get_argument('echostr')
-        # input your token
-        token = 'test'
-        #
+
         args_list = [token, timestamp, nonce]
         print(args_list)
         args_list.sort()
@@ -41,6 +44,14 @@ class WechatHandler(tornado.web.RequestHandler):
             self.write(echostr)
         else:
             self.write('fail ... ')
+
+    def post(self, *args, **kwargs):
+        print('URI', self.request.uri)
+        signature = self.get_argument('signature')
+        timestamp = self.get_argument('timestamp')
+        nonce = self.get_argument('nonce')
+        if wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+            print('POST pass: ', self.request.body)
 
 
 application = tornado.web.Application([
